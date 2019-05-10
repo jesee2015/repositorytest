d541cce4eda9bcd84e28f4311bb9d49a76e932d7
LiuXi <shakelx@qq.com>
2019-05-10 15:56:45 +0800

    firstAdd

---
+++ CoreSimple.Models/CashFlow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreSimple.Models
+{
+    public class CashFlow : ModelBase
+    {
+        /// <summary>
+        /// 增加
+        /// </summary>
+        public decimal Increase { get; set; }
+        /// <summary>
+        /// 减少
+        /// </summary>
+        public decimal Reduce { get; set; }
+        /// <summary>
+        /// 总金额
+        /// </summary>
+        public decimal Amount { get; set; }
+    }
+}
---
+++ CoreSimple.Models/ModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreSimple.Models
+{
+    public abstract class ModelBase
+    {
+        public int Id { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
